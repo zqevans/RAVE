5324d8bf54376f5eb59f8d8355141cfc761eeaee
Scott H. Hawley <scott.hawley@belmont.edu>
2022-08-08 20:53:42 -0500

    args all lowercase now

--- train_rave_accel.py
+++ train_rave_accel.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python3
+
 import accelerate
 import torch
 from torch.utils.data import DataLoader, random_split
@@ -22,11 +24,12 @@ if __name__ == "__main__":
 
 
     args = get_all_args()
+    print("args = ",args)
 
-    torch.manual_seed(args.SEED)
+    torch.manual_seed(args.seed)
 
     try:
-        mp.set_start_method(args.START_METHOD)
+        mp.set_start_method(args.start_method)
     except RuntimeError:
         pass
 
@@ -34,42 +37,42 @@ if __name__ == "__main__":
     device = accelerator.device
     print('Using device:', device, flush=True)
 
-    assert args.NAME is not None
-    model = RAVE(data_size=args.DATA_SIZE,
-                 capacity=args.CAPACITY,
-                 latent_size=args.LATENT_SIZE,
-                 ratios=args.RATIOS,
-                 bias=args.BIAS,
-                 loud_stride=args.LOUD_STRIDE,
-                 use_noise=args.USE_NOISE,
-                 noise_ratios=args.NOISE_RATIOS,
-                 noise_bands=args.NOISE_BANDS,
-                 d_capacity=args.D_CAPACITY,
-                 d_multiplier=args.D_MULTIPLIER,
-                 d_n_layers=args.D_N_LAYERS,
-                 warmup=args.WARMUP,
-                 mode=args.MODE,
-                 no_latency=args.NO_LATENCY,
-                 sr=args.SR,
-                 min_kl=args.MIN_KL,
-                 max_kl=args.MAX_KL,
-                 cropped_latent_size=args.CROPPED_LATENT_SIZE,
-                 feature_match=args.FEATURE_MATCH,
+    assert args.name is not None
+    model = RAVE(data_size=args.data_size,
+                 capacity=args.capacity,
+                 latent_size=args.latent_size,
+                 ratios=args.ratios,
+                 bias=args.bias,
+                 loud_stride=args.loud_stride,
+                 use_noise=args.use_noise,
+                 noise_ratios=args.noise_ratios,
+                 noise_bands=args.noise_bands,
+                 d_capacity=args.d_capacity,
+                 d_multiplier=args.d_multiplier,
+                 d_n_layers=args.d_n_layers,
+                 warmup=args.warmup,
+                 mode=args.mode,
+                 no_latency=args.no_latency,
+                 sr=args.sr,
+                 min_kl=args.min_kl,
+                 max_kl=args.max_kl,
+                 cropped_latent_size=args.cropped_latent_size,
+                 feature_match=args.feature_match,
                  device=accelerator.device)
 
     gen_opt, dis_opt = model.configure_optimizers()
 
     dataset = SimpleDataset(
-        args.PREPROCESSED,
-        args.WAV,
+        args.preprocessed,
+        args.wav,
         extension="*.wav,*.aif,*.flac",
-        preprocess_function=simple_audio_preprocess(args.SR,
-                                                    2 * args.N_SIGNAL),
+        preprocess_function=simple_audio_preprocess(args.sr,
+                                                    2 * args.n_signal),
         split_set="full",
         transforms=Compose([
-            RandomCrop(args.N_SIGNAL),
+            RandomCrop(args.n_signal),
             # RandomApply(
-            #     lambda x: random_phase_mangle(x, 20, 2000, .99, args.SR),
+            #     lambda x: random_phase_mangle(x, 20, 2000, .99, args.sr),
             #     p=.8,
             # ),
             Dequantize(16),
@@ -77,20 +80,20 @@ if __name__ == "__main__":
         ]),
     )
 
-    train = DataLoader(dataset, args.BATCH, True, drop_last=True, num_workers=8)
+    train = DataLoader(dataset, args.batch, True, drop_last=True, num_workers=8)
     
     model, gen_opt, dis_opt, train = accelerator.prepare(model, gen_opt, dis_opt, train)
 
     if accelerator.is_main_process:
-        x = torch.zeros(args.BATCH, 2**14).to(device)
+        x = torch.zeros(args.batch, 2**14).to(device)
         accelerator.unwrap_model(model).validation_step(x)
 
-    use_wandb = accelerator.is_main_process and args.NAME
+    use_wandb = accelerator.is_main_process and args.name
     if use_wandb:
         import wandb
         config = dict(args)
         #config['params'] = utils.n_params(model)
-        wandb.init(project=args.NAME, config=config, save_code=True)
+        wandb.init(project=args.name, config=config, save_code=True)
 
     if use_wandb:
         wandb.watch(model)
@@ -99,7 +102,7 @@ if __name__ == "__main__":
     epoch = 0
 
     try:
-        while step < args.MAX_STEPS:
+        while step < args.max_steps:
             for batch in tqdm(train, disable=not accelerator.is_main_process):
                 p = Profiler()
                 model_unwrap = accelerator.unwrap_model(model)
@@ -136,11 +139,11 @@ if __name__ == "__main__":
                     output = model_unwrap.validation_step(batch.detach())
                     p.tick("Validation")
 
-                    if step % args.VAL_EVERY == 0:
+                    if step % args.val_every == 0:
                         model_unwrap.validation_epoch_end([output])
                         p.tick("Demo")
 
-                # if step > 0 and step % args.CHECKPOINT_EVERY == 0:
+                # if step > 0 and step % args.checkpoint_every == 0:
                 #     save()
                 #print(p)
 
